9ef6009ebb2239f07a6adaa37898990a31144cc5
maemenaver <maemenaver@soongsil.ac.kr>
2021-08-29 02:21:02 +0900

    💄 Add Color Enum

---
+++ src/components/Color.ts
@@ -0,0 +1,9 @@
+enum Color {
+    BackgroundPrimary = "#FFFFFF",
+    BackgroundGradientStart = "#FFFFFF",
+    BackgroundGradientMid = "#FFFFFF",
+    BackgroundGradientEnd = "#FFFFFF",
+    TextPrimary = "#000000",
+}
+
+export default Color;
