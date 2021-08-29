4372bd296cd94d46b97b8ab88dd90e7dcb6640c7
maemenaver <maemenaver@soongsil.ac.kr>
2021-08-30 02:07:06 +0900

    🐛 Fix navigation undefined issue in NavigationBar storybook web

--- src/components/composites/NavigationBar/NavigationBar.tsx
+++ src/components/composites/NavigationBar/NavigationBar.tsx
@@ -9,14 +9,8 @@ import Color from "../../Color";
 import Text from "../../primitives/text/Text";
 
 /**
- * @description AccessoryLeft로 뒤로가기 버튼을 렌더하는 TopNavigation입니다. 이를 사용하는 스크린은 반드시 Drawer의 아래에서 렌더되어야 합니다
- * @param {boolean} transparent NavigationBar의 투명여부를 지정합니다. 기본값은 false입니다
- * @param {string} title transparent가 false인 경우에 표시할 타이틀 텍스트를 전달합니다. 기본값은 ""입니다
- * @param {boolean} renderLeftButton 의 왼쪽 버튼들을 렌더할지를 결정합니다. 기본값은 true입니다.
- * @param {boolean} renderRightButton 의 오른쪽 액션 버튼을 렌더할지 결정합니다. 기본값은 false입니다.
- * @param leftButtonType 의 왼쪽 액션 버튼의 타입을 결정합니다. 기본값은 "back" 입니다
- * @param rightButtonType 의 우측 액션 버튼의 타입을 지정합니다. 기본값은 "more"입니다.
- * @param {function | null} onRightButtonPress TopNavigation에서 우측버튼을 렌더할 경우 작성합니다. 완료, 더보기(...), 저장 등의 우측에 위치하는 버튼들의 기능을 전달하는 함수입니다.
+ * @description AccessoryLeft로 뒤로가기 버튼을 렌더하는 TopNavigation입니다.
+ * 이를 사용하는 스크린은 반드시 Drawer의 아래에서 렌더되어야 합니다
  */
 export default function NavigationBar({
     title = "",
@@ -27,8 +21,12 @@ export default function NavigationBar({
     onLeftPropPress,
     onRightPropPress,
 }: NavigationBaseProp) {
-    // Declare Navigation for Routing
-    const navigation = useNavigation();
+    let navigation;
+
+    try {
+        // Declare Navigation for Routing
+        navigation = useNavigation();
+    } catch (err) {}
 
     const defaultFunction = (navigationProp: NavigationPropEnum) => {
         switch (navigationProp) {
